daee0d0e721979f8ab7e38beb42f8180cc614b1d
Vitoria_Romero <123136691+VitoriaFalc@users.noreply.github.com>
2024-11-14 11:36:17 -0300

    Desafio 2
    Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
    
    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
    
    Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
    
    Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
    
    Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
+//exercicio 1
+/*Pergunte ao usuário qual é o dia da semana. 
+Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".*/
 
-    // Desafios da aula
-//Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
-alert('Boas vindas ao nosso site!');
-//Declare uma variável chamada nome e atribua a ela o valor "Lua".
-let nome = 'Lua';
-//Crie uma variável chamada idade e atribua a ela o valor 25
-let idade = 25;
-//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-let numeroDeVendas = 50;
-//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-let saldoDisponivel = 100;
-//Exiba um alerta com o texto "Erro! Preencha todos os campos"
-let mensagemDeErro = alert('Erro! Preencha todos os campos.');
-//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
-//Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
-nome = prompt('Qual o seu nome?');
-//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade
-idade = prompt('Qual a sua idade?');
-//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-if (idade >= 18){
-    alert('Pode tirar a habilitação');
+let diaSemana = prompt('Qual o dia da semana?');
+console.log(`O dia da semana é ${diaSemana}`);
+if (diaSemana == 'Sabado' || diaSemana == 'Domingo' ){
+    alert('Bom fim de semana!');
+} else{
+    alert('Boa Semana!');
 }
+//exercicio 2
+/*Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.*/
+
+let numero = prompt('Digite um numero');
+console.log(`O numerro digitado é ${numero}`);
+if (numero>0) {
+    alert('Numero Positivo');
+}if(numero == 0){
+    alert('Numero Neutro');
+}
+else {
+    alert('Numero negativo');
+}
+
+//exercicio3
+/*Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, 
+você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".*/
+
+let pontuacao = prompt('Digite sua pontuação');
+console.log(`A pontuação foi ${pontuacao}`);
+if (pontuacao >= 100){
+    alert('Parabens, você venceu!');
+} else {
+    alert('Tente novamente para ganhar.');
+}
+
+//exercicio4
+/*Crie uma mensagem que informa o usuário sobre o saldo da conta, 
+usando uma template string para incluir o valor do saldo.*/
+
+let saldo = "5.000";
+let usuario = 'Jonas';
+usuario = prompt('Digite seu nome');
+if (usuario == 'Jonas'){
+    alert('Seu sando é ' + saldo);
+} else {
+    alert('Não identificado.');
+}
+
+//exercicio5
+/*Peça ao usuário para inserir seu nome usando prompt. 
+Em seguida, mostre um alerta de boas-vindas usando esse nome.*/
+
+let nome = prompt('Qual seu nome');
+alert('Boas vindas ' + nome);
